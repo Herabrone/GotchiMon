a76b2ca31bcb0ae772ab79064dd9185a4dda7b9f
Darian Lagman <darjaylag@gmail.com>
2025-11-28 23:21:30 -0600

    ADDED: buttons to base

--- src/pages/Base.jsx
+++ src/pages/Base.jsx
@@ -1,9 +1,24 @@
-
+import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
 
 export default function Base() {
+
+    const navigate = useNavigate();
+
     return (
         <>
             <h1 style={{color: "black"}}>Base</h1>
+
+            <h1 style={{color: "black"}}>text box for dialogue</h1>
+            
+            <div className="action-container">
+                    <a onClick={() => {navigate("/")}} className="Feed_Button">Feed</a>
+            </div>
+            <div className="action-container">
+                    <a onClick={() => {navigate("/")}} className="Fight_Button">Fight</a>
+            </div>
+
+
         </>
     )
 }
