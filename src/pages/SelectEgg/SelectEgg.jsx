9eb77fb8dd81bcd3a42876f3c8a6f2c5060fcef8
K2bit <k2b1t20@gmail.com>
2025-11-29 09:59:48 -0600

    Select egg setup for images

--- src/pages/SelectEgg/SelectEgg.jsx
+++ src/pages/SelectEgg/SelectEgg.jsx
@@ -7,6 +7,12 @@ export default function SelectEgg() {
 
     const navigate = useNavigate();
 
+    const eggs = [
+        {name: "Egg 1", image: "image1"},
+        {name: "Egg 2", image: "image2"},
+        {name: "Egg 3", image: "image3"}
+    ]
+
     return (
         <ScreenLayout>
             <div className="page-container">
@@ -15,17 +21,13 @@ export default function SelectEgg() {
 
                 <div className="egg-select-container">
                     
-                    <div>
-                        Egg 1
-                    </div>
+                    {eggs.map((egg) => (
+                        <div className="egg-container">
+                            <span>{egg.image}</span>
+                            <span>{egg.name}</span>
+                        </div>
+                    ))}
 
-                    <div>
-                        Egg 2
-                    </div>
-
-                    <div>
-                        Egg 3
-                    </div>
                 </div>
                 <div className="action-container">
                     <a onClick={() => {navigate("/")}} className="egg-select-back">Nevermind</a>
