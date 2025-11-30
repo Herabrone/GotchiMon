81057ad1c31740d19663069edc626605f2c892cd
dmartin34-rrc <dmartin34@academic.rrc.ca>
2025-11-30 05:47:25 -0600

    Added: hopefully the last dialogue change

--- src/components/dialogueTree.js
+++ src/components/dialogueTree.js
@@ -147,7 +147,7 @@ const dialogueTree = {
         ],
     },
     AfterFirstEvolution_03: {
-        text: "YOU: Okay fine… this is getting expensive though",
+        text: "YOU: Okay fine, this is getting expensive though",
         options: [
             {text: "Continue", action: "goToEmptyShop", next: "AfterFirstEvolution_04"}
         ],
